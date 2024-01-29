6dd4fe5f0e4d6474129d3d4a90a7c6d503895562
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2024-01-29 13:42:54 +0300

    pipelines created and working

--- src/pipelines/full_pipeline.py
+++ src/pipelines/full_pipeline.py
@@ -2,6 +2,18 @@ from src.pipelines.data_pipeline import data_pipeline
 from src.pipelines.modelling_pipeline import modelling_pipeline
 from src.utils import load_config
 from logs import logger
+import mlflow
+from mlflow.tracking import MlflowClient
+# Constants
+EXPERIMENT_NAME_KEY = 'mlflow'
+METRIC_TO_USE = 'roc'
+MODEL_NAME = 'detect-ai-text-model'
+
+config = load_config()
+mlflow.set_experiment(config[EXPERIMENT_NAME_KEY]['experiment_name'])
+mlflow.set_tracking_uri(config[EXPERIMENT_NAME_KEY]['tracking_uri'])
+client = MlflowClient()
+
 
 def main(config):
     """
@@ -12,7 +24,7 @@ def main(config):
     data_pipeline(config['data_pipeline']['fit_tokenizer'],
                   config['data_pipeline']['fit_vectorizer'])
     # Modelling pipeline
-    modelling_pipeline(config['modelling_pipeline']['hyperparameter_tune'])
+    modelling_pipeline(client, config['modelling_pipeline']['hyperparameter_tune'])
     logger.info('Full pipeline finished')
 
 if __name__ == '__main__':
--- src/pipelines/modelling_pipeline.py
+++ src/pipelines/modelling_pipeline.py
@@ -7,34 +7,106 @@ from src.modelling.hyperparameters_tuning import objective
 from src.modelling.train import train_model
 from logs import logger
 import mlflow
+from mlflow.tracking import MlflowClient
 from src.utils import load_config
 from lightgbm import LGBMClassifier
 from xgboost import XGBClassifier
 
+# Constants
+EXPERIMENT_NAME_KEY = 'mlflow'
+METRIC_TO_USE = 'roc'
+MODEL_NAME = 'detect-ai-text-model'
+
 config = load_config()
+mlflow.set_experiment(config[EXPERIMENT_NAME_KEY]['experiment_name'])
+mlflow.set_tracking_uri(config[EXPERIMENT_NAME_KEY]['tracking_uri'])
+client = MlflowClient()
 
-mlflow.set_experiment(config['mlflow']['experiment_name'])
-mlflow.set_tracking_uri(config['mlflow']['tracking_uri'])
-PRODUCTION_TAG = 'production'
-
-
-def get_production_model_roc():
+def push_models_to_production(client):
     """
-    Fetch and return ROC of the model currently in production.
+    Compare the ROC of the model currently in production with the ROC of the best model.
 
     Returns:
-    - float: ROC of the model in production or 0 if no model is in production.
+    - None
     """
-    production_runs = mlflow.search_runs(
-        experiment_ids=[mlflow.get_experiment_by_name(config['mlflow']['experiment_name']).experiment_id],
-        filter_string=f"tags.mlflow.runName = '{PRODUCTION_TAG}'"
+    # Get the best model in a run
+    best_run, best_run_metric_value = get_best_run()
+
+    if not best_run:
+        logger.info("No runs found. Exiting...")
+        return
+
+    best_run_id = best_run.info.run_id
+
+    # Register the model
+    result = mlflow.register_model(f"runs:/{best_run_id}/model",
+                                   MODEL_NAME,
+                                   tags={METRIC_TO_USE: best_run_metric_value})
+
+    # Check for the best roc-recall results and push that model to production
+    model_version_details = client.get_latest_versions(MODEL_NAME, stages=["Production"])
+    
+    if model_version_details:
+        
+        production_roc_recall = float(model_version_details[0].tags.get(METRIC_TO_USE, 0.0))
+        if production_roc_recall < best_run_metric_value:
+            archive_current_model(model_version_details[0].version)
+            move_model_to_production(result.version)
+
+    
+
+
+def get_best_run():
+    """
+    Get the best model run based on ROC-recall.
+
+    Returns:
+    - Tuple(mlflow.entities.Run, float): Best model run and corresponding metric value.
+    """
+    best_run = client.search_runs(
+        experiment_ids=[mlflow.get_experiment_by_name(config[EXPERIMENT_NAME_KEY]['experiment_name']).experiment_id],
+        order_by=[f"metrics.{METRIC_TO_USE} DESC"],
+        max_results=1
+    )[0]
+    
+    
+    best_run_metric_value = float(best_run.data.metrics.get(METRIC_TO_USE, 0.0))
+    
+    return best_run, best_run_metric_value
+
+
+def archive_current_model(version):
+    """
+    Archive the model with the specified version.
+
+    Parameters:
+    - version: Model version to archive.
+
+    Returns:
+    - None
+    """
+    client.transition_model_version_stage(
+        name=MODEL_NAME,
+        version=version,
+        stage="Archived"
     )
-    if not production_runs.empty and 'roc' in production_runs.columns:
-        # Assuming 'roc' is the correct column name
-        return production_runs.iloc[0]['roc']
-    else:
-        print("No data or 'roc' column not found in the DataFrame.")
-        return 0.0
+
+
+def move_model_to_production(version):
+    """
+    Move the model with the specified version to production.
+
+    Parameters:
+    - version: Model version to move to production.
+
+    Returns:
+    - None
+    """
+    client.transition_model_version_stage(
+        name=MODEL_NAME,
+        version=version,
+        stage="Production"
+    )
 
 
 def train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyperparameter_tune):
@@ -54,55 +126,105 @@ def train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyper
     model = LGBMClassifier() if model_to_use == 'lgbm' else XGBClassifier()
 
     if hyperparameter_tune:
-        study = optuna.create_study(direction='maximize')
-        with mlflow.start_run():
-            if model_to_use == 'lgbm':
-                mlflow.lightgbm.autolog()
-            else:
-                mlflow.xgboost.autolog()
-
-            study.optimize(objective, n_trials=config['variables']['n_trials'])
-            best_params = study.best_params
-            logger.info("Best Hyperparameters:", best_params)
-            mlflow.log_params(best_params)
-
-        with open(config['models']['hyperparameters_path'], 'w') as json_file:
-            json.dump(best_params, json_file)
-
+        best_params = hyperparameter_tuning(X_train, y_train)
         model = train_model(X_train, y_train, model, best_params)
 
-    with open(config['models']['hyperparameters_path'], 'r') as json_file:
-        params = json.load(json_file)
-    
-    with mlflow.start_run():    	   
-        model = train_model(X_train, y_train, model, params)
+    else:
+        best_params = load_hyperparameters()
+        model = train_model(X_train, y_train, model, best_params)
+
+    with mlflow.start_run():
+        model = train_model(X_train, y_train, model, best_params)
 
         logger.info('Model trained')
 
-        # Evaluate the model on the validation set
-        logger.info('Evaluating model on the validation set...')
-        predictions = model.predict_proba(X_validation)[:, 1]
-        roc = roc_auc_score(y_validation, predictions)
-        custom_metric_value = custom_metric(y_validation, predictions)
+        evaluate_and_log_metrics(model, X_validation, y_validation)
 
-        mlflow.log_metric("roc", roc)
-        mlflow.log_metric("custom_metric", custom_metric_value)
-        if model_to_use == 'lgbm':
-            mlflow.lightgbm.log_model(model, 'model')
-        else:
-            mlflow.xgboost.log_model(model, 'model')
+        log_model(model, model_to_use)
+
+    return model, roc_auc_score(y_validation, model.predict_proba(X_validation)[:, 1])
+
+
+def hyperparameter_tuning(X_train, y_train):
+    """
+    Perform hyperparameter tuning and return the best parameters.
+
+    Parameters:
+    - X_train, y_train: Training data and labels.
+
+    Returns:
+    - dict: Best hyperparameters.
+    """
+    study = optuna.create_study(direction='maximize')
+    with mlflow.start_run():
+        study.optimize(objective, n_trials=config['variables']['n_trials'])
+        best_params = study.best_params
+        logger.info("Best Hyperparameters:", best_params)
+        mlflow.log_params(best_params)
+        mlflow.log_metrics(METRIC_TO_USE, study.best_value)
+
+    with open(config['models']['hyperparameters_path'], 'w') as json_file:
+        json.dump(best_params, json_file)
+
+    return best_params
+
+
+def load_hyperparameters():
+    """
+    Load hyperparameters from a file.
+
+    Returns:
+    - dict: Loaded hyperparameters.
+    """
+    with open(config['models']['hyperparameters_path'], 'r') as json_file:
+        return json.load(json_file)
+
+
+def evaluate_and_log_metrics(model, X_validation, y_validation):
+    """
+    Evaluate the model on the validation set and log metrics.
+
+    Parameters:
+    - model: Trained model.
+    - X_validation, y_validation: Validation data and labels.
+
+    Returns:
+    - None
+    """
+    logger.info('Evaluating model on the validation set...')
+    predictions = model.predict_proba(X_validation)[:, 1]
+    roc = roc_auc_score(y_validation, predictions)
+    custom_metric_value = custom_metric(y_validation, predictions)
+
+    mlflow.log_metric("roc", roc)
+    mlflow.log_metric("roc_recall", custom_metric_value)
 
     logger.info(f'The ROC on the validation set is {roc} and the custom metric is {custom_metric_value}.')
 
-    return model, roc
+
+def log_model(model, model_to_use):
+    """
+    Log the trained model.
+
+    Parameters:
+    - model: Trained model.
+    - model_to_use: Model type ('lgbm' or 'xgboost').
+
+    Returns:
+    - None
+    """
+    if model_to_use == 'lgbm':
+        mlflow.lightgbm.log_model(model, 'model')
+    else:
+        mlflow.xgboost.log_model(model, 'model')
 
 
-
-def modelling_pipeline(hyperparameter_tune: bool = False) -> None:
+def modelling_pipeline(client, hyperparameter_tune: bool = False) -> None:
     """
     Execute the modelling pipeline.
 
     Parameters:
+    - client: MlflowClient instance.
     - hyperparameter_tune (bool): Whether to tune the hyperparameters.
 
     Returns:
@@ -120,35 +242,18 @@ def modelling_pipeline(hyperparameter_tune: bool = False) -> None:
     y_validation = load_data(config['data']['modelling_y_validation_data_path'])
     logger.info('Data loaded')
     
-    # Check if a model is in production
-    production_roc = get_production_model_roc()
-
-    if production_roc > 0:
-        # Model is in production, evaluate new model on validation set
-        logger.info('Model in production. Evaluating new model...')
-        new_model, new_roc = train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyperparameter_tune)
-
-        # Check if new model has better ROC than the model in production
-        if new_roc > production_roc:
-            # New model has better performance, move it to production
-            logger.info('New model has better performance. Pushing to production...')
-            mlflow.set_tag('mlflow.runName', PRODUCTION_TAG)
-            mlflow.sklearn.log_model(new_model, "model")
-        else:
-            logger.info('New model does not have better performance than the model in production.')
-
+    # If hyperparameter tune, hyperparameterize the model
+    if hyperparameter_tune:
+        logger.info('Hyperparameter tuning...')
+        model, _ = train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyperparameter_tune)
+        push_models_to_production(client)
+        logger.info('Hyperparameter tuning completed')
     else:
-        # No model in production, train and push the model to production
-        logger.info('No model in production. Training and pushing the model to production...')
-        new_model, _ = train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyperparameter_tune)
-
-        # Push the model to production
-        logger.info('Pushing the model to production...')
-        mlflow.set_tag('mlflow.runName', PRODUCTION_TAG)
-        mlflow.sklearn.log_model(new_model, "model")
-
-    logger.info('Modelling pipeline finished')
+        logger.info('Training model...')
+        model, _ = train_and_evaluate_model(X_train, y_train, X_validation, y_validation, hyperparameter_tune)
+        push_models_to_production(client)
+        logger.info('Model trained')
 
 
 if __name__ == '__main__':
-    modelling_pipeline(config['modelling_pipeline']['hyperparameter_tune'])
+    modelling_pipeline(client, config['modelling_pipeline']['hyperparameter_tune'])
