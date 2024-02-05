68cd57882b621185fac9d6a14257ac6aefcd00c3
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2024-02-05 15:46:26 +0300

    edited app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI, HTTPException, Depends
+import mlflow
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-import mlflow.pyfunc
-import pandas as pd
-from typing import List
+from mlflow.exceptions import MlflowException
 from pydantic import BaseModel
 from src.utils import load_config
 from src.pipelines.full_pipeline import main
@@ -11,26 +10,27 @@ from src.inference.model_inference import get_inference
 app = FastAPI()
 config = load_config()
 
-# Define the path to the logged model
-logged_model = config["models"]["model_uri"]
-
-# Load the model as a PyFuncModel
-loaded_model = mlflow.pyfunc.load_model(logged_model)
-
 class PredictionInput(BaseModel):
     text: str
 
 class TrainingInput(BaseModel):
     train: bool
 
+# Define the path to the logged model
+logged_model = config["models"]["model_uri"]
 
-@app.get('/predict/{text}')
+def load_mlflow_model(model_uri: str):
+    try:
+        return mlflow.pyfunc.load_model(model_uri)
+    except MlflowException as e:
+        raise HTTPException(status_code=500, detail=f"Error loading MLflow model: {str(e)}")
+
+loaded_model = load_mlflow_model(logged_model)
+
+@app.get('/predict/')
 async def predict(text: str):
     try:
-        # Perform inference using the loaded model
         predictions = get_inference(text)
-
-        # Return the predictions as JSON
         return JSONResponse(content={
             'predictions': predictions.tolist(),
             'predictions_class': 'AI' if predictions[0] == 1 else 'Not AI'
@@ -41,9 +41,8 @@ async def predict(text: str):
 @app.post('/train')
 async def train(input_data: TrainingInput):
     try:
-        if input_data:
+        if input_data.train:
             main(config)
-
         return {'status': 'Training completed successfully'}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
